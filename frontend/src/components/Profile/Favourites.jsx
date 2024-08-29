05d2f131b98c0cbd8630d01371f2986e2409fe55
chamudi <chamuvithanage@gmail.com>
2024-08-29 23:59:23 +0530

    profile

--- frontend/src/components/Profile/Favourites.jsx
+++ frontend/src/components/Profile/Favourites.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react'
 import axios from 'axios';
+import BookCard from "../BookCard/BookCard";
+
 
 const Favourites = () => {
+  const [FavouriteBooks,setFavouriteBooks]=useState();
   const headers={
     id:localStorage.getItem("id"),
     authorization:`Bearer ${localStorage.getItem("token")}`,
@@ -10,13 +13,21 @@ const Favourites = () => {
   useEffect(()=>{
     const fetch=async()=>{
       const response=await axios.get("http://localhost:1000/api/v1//get-fav-books",{headers});
-      console.log(response.data.data);
+      setFavouriteBooks(response.data.data);
     };
     fetch();
     
   },[]);
-  return <div>Favourites</div>
-  
+  return(
+  <div>
+    {FavouriteBooks && FavouriteBooks.map((items,i)=>(
+      <div key={i}>
+      <BookCard data={items}/>
+      </div>
+    ))}
+  </div>
+  );
+
 };
 
 export default Favourites;
