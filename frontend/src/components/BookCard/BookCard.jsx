1648fe54dc78454c11aa50f550ca298b87d48bcc
chamudi <chamuvithanage@gmail.com>
2024-08-30 21:02:23 +0530

    delete from fav

--- frontend/src/components/BookCard/BookCard.jsx
+++ frontend/src/components/BookCard/BookCard.jsx
@@ -1,18 +1,44 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
-const BookCard = ({data}) => {
-    console.log(data);
-  return (<>
+const BookCard = ({data,favourite}) => {
+  const headers={
+    id:localStorage.getItem("id"),
+    authorization:`Bearer ${localStorage.getItem("token")}`,
+    bookid:data._id,
+  };
+  const handleRemovebook=async()=>{
+    const response=await axios.put("http://localhost:1000/api/v1/remove-book-from-fav",{},
+      {headers}
+    );
+    alert(response.data.message);
+    
+
+  };
+    
+  return (
+    <div className='bg-zinc-800 rounded p-4 flex flex-col'>
    <Link to ={`/view-book-details/${data._id}`}>
+   
    <div className='bg-zinc-800 rounded border border-yellow-100 p-2 flex flex-col '>
-     <div className='bg-zinc-900 rounded flex items-center justify-center'><img src={data.url} alt='/' className='h-[28vh]'/>
+     <div className='bg-zinc-900 rounded flex items-center justify-center'>
+      <img src={data.url} alt='/' className='h-[28vh]'/>
      </div>
      <h2 className='mt-4 text-xl text-zinc-200 font-semibold'>{data.title}</h2>
      <p className='mt-2 text-zinc-400 font-semibold '>by {data.author}</p>
      <p className='mt-2 text-zinc-200 font-semibold '> Rs. {data.price}</p>
+      </div>
+    </Link>
+    {favourite && (
+    <button 
+      className='bg-zinc-500  px-4 py-2 rounded border border-yellow-500 text-black hover:bg-blue-200  mt-4' 
+      onClick={handleRemovebook}>
+      Remove from Favourites
+      </button>
+    )}
     </div>
-    </Link></>
+  
   );
   
 };
--- frontend/src/components/Profile/Favourites.jsx
+++ frontend/src/components/Profile/Favourites.jsx
@@ -5,27 +5,36 @@ import BookCard from "../BookCard/BookCard";
 
 
 const Favourites = () => {
-  const [FavouriteBooks,setFavouriteBooks]=useState();
+  const [FavouriteBooks,setFavouriteBooks]=useState([]);
   const headers={
     id:localStorage.getItem("id"),
     authorization:`Bearer ${localStorage.getItem("token")}`,
   };
   useEffect(()=>{
     const fetch=async()=>{
-      const response=await axios.get("http://localhost:1000/api/v1//get-fav-books",{headers});
+      const response=await axios.get("http://localhost:1000/api/v1/get-fav-books",{headers});
       setFavouriteBooks(response.data.data);
     };
     fetch();
     
-  },[]);
+  },[FavouriteBooks]);
   return(
-  <div>
-    {FavouriteBooks && FavouriteBooks.map((items,i)=>(
-      <div key={i}>
-      <BookCard data={items}/>
+    <>
+    {FavouriteBooks && FavouriteBooks.length===0 && (
+      <div className='flex flex-col text-2xl font-semibold h-[100%] text-zinc-800 flex items-center justify-center w-full bg-zinc-700'>
+        <img src='remove.png' className='h-[20vh] my-4'/> 
+      <p >No favourite Books</p>
+      </div>)}
+      
+      <div className='grid grid-cols-4 gap-4'>
+        {FavouriteBooks && FavouriteBooks.map((items,i)=>(
+        <div key={i}>
+        <BookCard data={items} favourite={true}/>
+        </div>
+      ))}
       </div>
-    ))}
-  </div>
+    </>
+  
   );
 
 };
