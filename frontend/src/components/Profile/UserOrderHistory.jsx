f59165e90eeb3b41f5f6c3e9bf64589e2f87bbbd
chamudi <chamuvithanage@gmail.com>
2024-09-01 23:22:21 +0530

    order

--- frontend/src/components/Profile/UserOrderHistory.jsx
+++ frontend/src/components/Profile/UserOrderHistory.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import Loader from "../Loader/Loader";
 
 const UserOrderHistory = () => {
+  const[orderHistory,setOrderHistory]= useState();
+  const headers={
+    id:localStorage.getItem("id"),
+    authorization:`Bearer ${localStorage.getItem("token")}`,
+  };
+  useEffect(()=>{
+    const fetch=async() => {
+      const response=await axios.get(
+        "http://localhost:1000/api/v1/get-order-history",{headers}
+      );
+      setOrderHistory(response.data.data);
+    };
+    fetch();
+  },[]);
   return (
-    <div>User</div>
+    <>
+    {!orderHistory && <div className='flex items-center justify-center h-[100%]'><Loader/></div>}
+    {orderHistory && orderHistory.length===0 &&(
+      <div className='h-[80vh] p-4 text-zinc-100'>
+        <div className='h-[100%] flex flex-col items-center justify-center'>
+          <h1 className='text-5xl font-semibold text-zinc-500 mb-8'>
+            No Order History
+          </h1>
+          <img 
+          src='noorder.png' alt='' className='h-[20vh] mb-8'/>
+        </div>
+      </div>
+    )}
+    </>
   )
 }
 
