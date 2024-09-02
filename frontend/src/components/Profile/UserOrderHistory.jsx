ac5b459ae6d2095bdd1b16d68a46ac5cd10fdf2a
chamudi <chamuvithanage@gmail.com>
2024-09-02 12:15:14 +0530

    order

--- frontend/src/components/Profile/UserOrderHistory.jsx
+++ frontend/src/components/Profile/UserOrderHistory.jsx
@@ -19,7 +19,7 @@ const UserOrderHistory = () => {
   },[]);
   return (
     <>
-    {!orderHistory && <div className='flex items-center justify-center h-[100%]'><Loader/></div>}
+    {!orderHistory && <div className='flex items-center justify-center h-[100%]cd'><Loader/></div>}
     {orderHistory && orderHistory.length===0 &&(
       <div className='h-[80vh] p-4 text-zinc-100'>
         <div className='h-[100%] flex flex-col items-center justify-center'>
@@ -29,6 +29,62 @@ const UserOrderHistory = () => {
           <img 
           src='noorder.png' alt='' className='h-[20vh] mb-8'/>
         </div>
+      </div>
+    )}
+    {orderHistory && orderHistory.length >0 &&(
+      <div className='h[100%] p-0 md:p-4 text-zinc-100 '>
+        <h1 className='text-3xl md:text-5xl font-semibold text-zinc-500 mb-8'>
+          Your Order history
+        </h1>
+        <div className='mt-4 bg-zinc-800 w-full rounded py-2 px-4 flex gap-2'>
+          <div className='w-[3%]'>
+            <h1 className='text-center'>Sr .</h1>
+          </div>
+          <div className='w-[22%]'>
+            <h1 className=''>Books</h1>
+          </div>
+          <div className='w-[45%]'>
+            <h1 className=''>Description</h1>
+          </div>
+          <div className='w-[9%]'>
+            <h1 className=''>Price</h1>
+          </div>
+          <div className='w-[16%]'>
+            <h1 className=''>Status</h1>
+          </div>
+          <div className='w-none md:w-[5%] hidden md:block'>
+            <h1 className=''>Mode</h1>
+          </div>
+        </div>
+        {orderHistory.map((items,i)=>(
+          <div className='bg-zinc-800 w-full rounded py-2 px-4 flex gap-4 hover:bg-zinc-900 hover:cursor-pointer'>
+            <div className='w-[3%]'>
+              <h1 className='text-center'>{i+1}</h1>
+            </div>
+            <div className='w-[22%]'>
+              <link
+              to={`/view-book-details/${items.book._id}`}
+              className='hover:text-blue-300'>{items.book.title}</link>
+            </div>
+            <div className='w-[45%]'>
+              <h1 className=''>{items.book.desc.slice(0,50)}...</h1>
+            </div>
+            <div className='w-[9%]'>
+              <h1 className=''>{items.book.price}</h1>
+            </div>
+            <div className='w-[16%]'>
+              <h1 className='font-semibold text-green-500'>
+                {items.status==="Order placed" ?(
+                  <div className='text-yellow-500'>{items.status}</div>
+                ): items.status ==="canceled" ?(
+                  <div className='text-red-500'>{items.status}</div>
+                ):(
+                  items.status
+                )}
+                </h1>
+            </div>
+          </div>
+        ))}
       </div>
     )}
     </>
